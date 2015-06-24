f0661301a5bb725bc091d1b8cbbcc9aa9ae79d25
Yosnier Samon <yosamac@gmail.com>
2015-06-24 23:20:01 +0200

    Added CRUD operations

--- collectionDriver.js
+++ collectionDriver.js
@@ -1,3 +1,4 @@
+var ObjectID = require ('mongodb').ObjectID;
 CollectionDriver = function(db){
 	this.db = db;
 }
@@ -23,20 +24,60 @@ CollectionDriver.prototype.findAll = function (collectionName,callback){
 
 CollectionDriver.prototype.get = function (collectionName, id, callback){
 	this.getCollection(collectionName,function(error, the_collection){
-		if (error) 
-			callback (error);
+		if (error)
+		callback (error);
 		else {
 			var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
-			if (checkForHexRegExp.test(id)) 
-				callback({error:"invalid id"});
-			else 
-				the_collection.findOne({'_id':ObjectID(id)},function(error,doc){
-					if (error) 
-						callback(error);
-					else 
-						callback(null,doc);
-				});
+			if (checkForHexRegExp.test(id))
+			callback({error:"invalid id"});
+			else
+			the_collection.findOne({'_id':ObjectID(id)},function(error,doc){
+			  	if (error) callback(error);
+				else callback(null,doc);
+			});
 		}
 	});
 };
-exports.CollectionDriver = CollectionDriver;
+//save new object
+CollectionDriver.prototype.save = function(collectionName,obj,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if (error) callback(error)
+		else{
+			obj.created_at = new Date();
+			the_collection.insert(obj,function(){
+				callback(null,obj);
+			});
+		}
+	});
+};
+// update specific object
+CollectionDriver.prototype.update = function(collectionName,obj,entityId,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if (error) callback(error)
+		else{
+			obj._id = ObjectID(entityId);
+			obj.update_at = new Date();
+			the_collection.save(obj,function(error,doc){
+				if (error) callback(error);
+				else callback(null,obj);
+			});
+		}
+	});
+};
+// delete a specific object
+CollectionDriver.prototype.delete = function(collectionName,entityId,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if (error) callback(error)
+		else{
+			the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc){
+				if (error) callback(error);
+				else callback(null,doc);
+			});
+		}
+	});
+};
+
+
+
+
+	exports.CollectionDriver = CollectionDriver;
--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 var http = require('http'),
 	express = require('express'),
-	path = require('path')
+	methodOverride = require('method-override'),
+	bodyParser = require('body-parser'),
+	errorhandler = require('errorhandler'),
+	path = require('path'),
 	MongoClient = require ('mongodb').MongoClient,
 	Server = require('mongodb').Server,
-	CollectionDriver = require('./collectionDriver').collectionDriver;
-
-
+	CollectionDriver = require('./collectionDriver').CollectionDriver;
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'anabiServer/views'));
+app.set('views', path.join(__dirname,'views'));
 app.set('view engine','jade');
+//app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.json());
 
-var mongoHost='localHost';
+
+var mongoHost = 'localHost'; //A
 var mongoPort = 27017;
 var collectionDriver;
 
 var mongoClient = new MongoClient(new Server(mongoHost,mongoPort));
-mongoClient.prototype.open(function(err,mongoClient){
+mongoClient.open(function(err,mongoClient){
 	if (!mongoClient) {
 		console.error('Error! Existing... Must start MongoDB first');
 		process.exit(1);
 	}
-	var db = mongoClient.db("db");
+	var db = mongoClient.db("anabi");
 	collectionDriver = new CollectionDriver(db);
 });
 
-app.use(express.static(path.join(__dirname,'anabiServer/public')));
-
+app.use(express.static(path.join(__dirname,'public')));
 // app.get('/', function(req,res){
 // 	res.send('<html><body>Hello world</body></html>');
 // });
 
 app.get ('/:collection',function(req,res){
 	var params = req.params;
-	collectionDriver.findAll(params.collection,function(error,objs){
+	collectionDriver.findAll(req.params.collection,function(error,objs){
 		if (error) {
 			res.send(400,error);
 		}else {
 			if (req.accepts('html')) {
-				res.render('data',{objects:objs, collection:params.collection});
+				res.render('data',{objects:objs, collection:req.params.collection});
 			} else {
 				res.set('Content-type','application/json');
 				res.send(200,objs);
@@ -47,7 +51,6 @@ app.get ('/:collection',function(req,res){
 		}
 	});
 });
-
 
 app.get ('/:collection/:entity',function(req,res){
 	var params = req.params;
@@ -67,9 +70,56 @@ app.get ('/:collection/:entity',function(req,res){
 	}
 });
 
-
 app.get('/:a?/:b?/:c?', function (req,res) {
 	res.send(req.params.a+' '+req.params.b+' '+req.params.c);
+});
+
+app.post('/:collection', function(req,res){
+	var object = req.body;
+	var collection = req.params.collection;
+	collectionDriver.save(collection,object, function(err,docs){
+		if (err) {
+			res.send(400,err);
+		} else {
+			res.send(201,docs);
+		}
+	});
+});
+// update method
+app.put('/:collection/:entity', function(req,res){
+	var params = req.params,
+	 	 entity = params.entity,
+		 collection = params.collection;
+		if (entity) {
+			collectionDriver.update(collection, req.body, entity, function(err,objs){
+				if (err) {
+					res.send(400,err);
+				} else {
+					res.send(201,objs);
+				}
+			});
+		} else {
+			var error = {"message":"Cannot PUT a whole collection"};
+			res.send(400,error);
+		}
+});
+// delete method
+app.delete('/:collection/:entity', function(req,res){
+	var params = req.params,
+	 	 entity = params.entity,
+		 collection = params.collection;
+		if (entity) {
+			collectionDriver.delete(collection, entity, function(err,objs){
+				if (err) {
+					res.send(400,err);
+				} else {
+					res.send(200,objs);
+				}
+			});
+		} else {
+			var error = {"message":"Cannot DELETE a whole collection"};
+			res.send(400,error);
+		}
 });
 
 app.use(function(req,res){
